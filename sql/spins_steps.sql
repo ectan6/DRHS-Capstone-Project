c458cebbfa2ab1dd622751a6f669b945b72c991c
Eliza Tan <elizacarrietan2006@gmail.com>
2024-01-26 00:26:53 -0500

    I created a table where my spins and steps will go.

---
+++ sql/spins_steps.sql
@@ -0,0 +1,15 @@
+-- main.spins_steps definition
+
+-- Drop table
+
+-- DROP TABLE main.spins_steps;
+
+CREATE TABLE main.spins_steps (
+	"name" varchar NULL,
+	code varchar NULL,
+	lb float4 NULL,
+	l1 float4 NULL,
+	l2 float4 NULL,
+	l3 float4 NULL,
+	l4 float4 NULL
+);
--- sql/test.sql
+++ sql/test.sql
@@ -4,9 +4,6 @@
 
 -- DROP TABLE main.test;
 
--- Eliza is changing this comment
--- Another test
-
 CREATE TABLE main.test (
 	id int4 NULL,
 	"name" varchar NULL,
