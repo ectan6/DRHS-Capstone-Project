25ed00f42dd61b2453465bed8992bd026403615e
Eliza Tan <elizacarrietan2006@gmail.com>
2024-01-28 15:54:15 -0500

    I created a table named "jumps" that holds the values of each jump

---
+++ sql/jumps.sql
@@ -0,0 +1,13 @@
+-- main.jumps definition
+
+-- Drop table
+
+-- DROP TABLE main.jumps;
+
+CREATE TABLE main.jumps (
+	"element" varchar NULL,
+	code varchar NULL,
+	sov float4 NULL,
+	under_or_e float4 NULL,
+	under_and_e float4 NULL
+);
