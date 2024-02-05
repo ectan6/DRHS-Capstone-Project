10b7df778918a8fd0c534339dfb8c7aa6aca9f2e
Eliza Tan <elizacarrietan2006@gmail.com>
2024-02-05 17:55:12 -0500

    making a landing page that asks for user type, username, and password

--- main.py
+++ main.py
@@ -20,5 +20,36 @@ with engine.connect() as conn:
     df = pd.read_sql("select * from main.test", conn)
     # print(rows)
 
+st.title("Club Judging System!")
 
-st.dataframe(df)
+# login = st.button("Login", type="primary")
+
+if 'stage' not in st.session_state:
+    st.session_state.stage = 0
+
+def set_state(i):
+    st.session_state.stage = i
+
+if st.session_state.stage == 0:
+    st.button('Login', on_click=set_state, args=[1])
+
+if st.session_state.stage >= 1:
+    user_type = st.selectbox(
+        'I am a:',
+        [None, 'skater', 'technical specialist', 'judge'],
+        on_change=set_state, args=[2]
+    )
+    if user_type is None:
+        set_state(1)
+
+if st.session_state.stage >= 2:
+    user = st.text_input('username', on_change=set_state, args=[3])
+
+if st.session_state.stage >=3:
+    password = st.text_input('password', on_change=set_state, args=[4])
+
+if st.session_state.stage >= 4:
+    st.write(f'Hello {user}!')
+    st.button('Start Over', on_click=set_state, args=[0])
+
+# st.dataframe(df)
