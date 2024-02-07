755a5f58a1f23b1b2b5c4ef02779fec23bcc4e2f
Eliza Tan <elizacarrietan2006@gmail.com>
2024-02-07 00:57:14 -0500

    remade the jumps table with the code as the primary key

--- sql/jumps.sql
+++ sql/jumps.sql
@@ -5,9 +5,10 @@
 -- DROP TABLE main.jumps;
 
 CREATE TABLE main.jumps (
+	code varchar NOT NULL,
 	"element" varchar NULL,
-	code varchar NULL,
 	sov float4 NULL,
 	under_or_e float4 NULL,
-	under_and_e float4 NULL
+	under_and_e float4 NULL,
+	CONSTRAINT jumps_pk PRIMARY KEY (code)
 );
