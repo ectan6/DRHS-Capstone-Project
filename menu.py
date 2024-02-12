ccd74d4ac6a58acc328760d2f583065a70ffb173
Eliza Tan <elizacarrietan2006@gmail.com>
2024-02-12 15:16:35 -0500

    deleting repeted code (oops)

--- menu.py
+++ menu.py
@@ -20,8 +20,7 @@ def unauthenticated_menu():
 
 
 def menu():
-    # Determine if a user is logged in or not, then show the correct
-    # navigation menu
+    # Determine if a user is logged in or not, then show the correct navigation menu
     if "role" not in st.session_state or st.session_state.role is None:
         unauthenticated_menu()
         return
@@ -29,22 +28,7 @@ def menu():
 
 
 def menu_with_redirect():
-    # Redirect users to the main page if not logged in, otherwise continue to
-    # render the navigation menu
+    # Redirect users to the main page if not logged in, otherwise continue to render navigation menu
     if "role" not in st.session_state or st.session_state.role is None:
         st.switch_page("app.py")
     menu()
-
-
-def authenticated_menu():
-    # Show a navigation menu for authenticated users
-    st.sidebar.page_link("app.py", label="Switch accounts")
-    st.sidebar.page_link("pages/user.py", label="Your profile")
-    if st.session_state.role in ["admin", "super-admin"]:
-        st.sidebar.page_link("pages/admin.py", label="Manage users")
-        st.sidebar.page_link(
-            "pages/super-admin.py",
-            label="Manage admin access",
-            disabled=st.session_state.role != "super-admin",
-        )
-
