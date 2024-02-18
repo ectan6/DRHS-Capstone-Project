cde0dadadf1438f9643ac99b85f958fc02d1fb6e
Eliza Tan <elizacarrietan2006@gmail.com>
2024-02-18 13:55:12 -0500

    got rid of old sql comments

--- pages/2_Technical_Specialist.py
+++ pages/2_Technical_Specialist.py
@@ -26,11 +26,8 @@ st.markdown(f"You are currently logged with the role of {st.session_state.role}.
 # connect to the database and printing connection successful if connected
 with engine.connect() as conn:
     print("Connection successful")
-    # sql_text = text("select * from main.test")
-    # result = conn.execute(sql_text)
-    # rows = result.fetchall()
     # dataframe!!!
     df = pd.read_sql("select * from main.jumps", conn)
-    # print(rows)
 
 st.dataframe(df)
+
