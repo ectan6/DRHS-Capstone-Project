aca627b3499ef2039d801e7a42c91c48f32badca
Eliza Tan <elizacarrietan2006@gmail.com>
2024-02-26 17:32:30 -0500

    trying index? for loc

--- pages/2_Technical_Specialist.py
+++ pages/2_Technical_Specialist.py
@@ -23,27 +23,12 @@ st.title("tech specialist screen")
 st.markdown(f"You are currently logged with the role of {st.session_state.role}.")
 
 # make fnctn w parameters -> one thing to update lots of stuff
-# def click_button(program_id: int, element_id: str, counter: int):
-#     # connect to the database
-#     with engine.connect() as conn:
-#         # dataframe!!!
-#         old_df = pd.read_sql("select * from main.test_program", conn)
-#         old_df.loc[counter, :] = [program_id, element_id, counter]
-#         print(old_df)
-#         sql_call = old_df.to_sql(name='test_program', schema='main', con=conn, if_exists='replace', index=False)
-#         print(sql_call)
-#         conn.commit()
-#         print(old_df)
-
-
-
 def click_button(score_id: int, jump_id: str, spin_id: str, order_executed: int):
     # connect to the database
     with engine.connect() as conn:
         # dataframe!!!
         old_df = pd.read_sql("select * from main.score", conn)
-        # calculate score
-        old_df.loc[1, :] = [score_id, jump_id, spin_id, order_executed]
+        old_df.loc[len(old_df.index)] = [score_id, jump_id, spin_id, order_executed]
         print(old_df)
         sql_call = old_df.to_sql(name='score', schema='main', con=conn, if_exists='replace', index=False)
         print(sql_call)
@@ -51,7 +36,7 @@ def click_button(score_id: int, jump_id: str, spin_id: str, order_executed: int)
         print(old_df)
 
 st.title("jumps")
-col1, col2, col3, col4, col5, col6 = st.columns(6)
+col1, col2, col3, col4, col5 = st.columns(5)
 
 with col1:
     st.write("toeloop")
@@ -60,20 +45,19 @@ with col2:
     # if button clicked inside here instead of session state
     # ex:
     if st.button("1"):
-        click_button(1, "1T", "", 1)
-        # st.dataframe(df)
+        click_button(2, "1T", "", 2)
 
-# with col3:
-    # if st.button("2"):
-        # click_button(15, "1T", 7)
+with col3:
+    if st.button("2"):
+        click_button(15, "1T","", 7)
 
-# with col4:
-    # if st.button("3"):
-        # click_button(44, "1T", 8)
+with col4:
+    if st.button("3"):
+        click_button(44, "1T", "", 8)
 
-# with col5:
-    # if st.button("4"):
-        # click_button(55, "1T", 9)
+with col5:
+    if st.button("4"):
+        click_button(55, "1T", "", 9)
 
 # with col6:
 #     df_completed_elements = pd.DataFrame(
