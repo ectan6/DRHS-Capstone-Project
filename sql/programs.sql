c1972aa79f6741691daecfec4478c096f10f6a18
Eliza Tan <elizacarrietan2006@gmail.com>
2024-03-26 23:42:05 -0400

    fixing up some foreign keys

--- sql/programs.sql
+++ sql/programs.sql
@@ -14,4 +14,5 @@ CREATE TABLE main.programs (
 
 -- main.programs foreign keys
 
+ALTER TABLE main.programs ADD CONSTRAINT programs_competitions_fk FOREIGN KEY (competition_id) REFERENCES main.competitions(competition_id);
 ALTER TABLE main.programs ADD CONSTRAINT programs_users_fk FOREIGN KEY (user_id) REFERENCES main.users(user_id);
