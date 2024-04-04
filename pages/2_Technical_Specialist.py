0c800099e2e0b700956f320df259a52d2af07de3
Eliza Tan <elizacarrietan2006@gmail.com>
2024-04-04 19:27:02 -0400

    highlight example!

--- pages/2_Technical_Specialist.py
+++ pages/2_Technical_Specialist.py
@@ -359,5 +359,16 @@ st.dataframe(
     },
 )
 
+if st.button("submit", key="submit-techspecialist"):
+    st.snow()
 
-# if st.button("submit", key="submit-techspecialist"):
+
+# streamlit example for highlighting dataframe: ---------
+
+# import streamlit as st
+# import pandas as pd
+# import numpy as np
+
+# df = pd.DataFrame(np.random.randn(10, 20), columns=("col %d" % i for i in range(20)))
+
+# st.dataframe(df.style.highlight_max(axis=0))
